6e1cecb663143903700d151ae2709d0443e81c01
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-09-28 13:29:15 +0200

    refactor: Update Unity component and add overlay buttons
    This commit refactors the Unity component in the App.tsx file. It updates the URLs for the loader, data, framework, and code files to use the "HackBuild" version instead of the "Experiment" version. Additionally, it adds overlay buttons for left and right actions, which send messages to the "NetworkBridge" in the Unity component.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
 import { Unity, useUnityContext } from 'react-unity-webgl'
 
 function App() {
-  const { unityProvider } = useUnityContext({
-    loaderUrl: 'Build/Experiment.loader.js',
-    dataUrl: 'Build/Experiment.data.gz',
-    frameworkUrl: 'Build/Experiment.framework.js.gz',
-    codeUrl: 'Build/Experiment.wasm.gz'
+  const { unityProvider, sendMessage } = useUnityContext({
+    loaderUrl: 'Build/HackBuild.loader.js',
+    dataUrl: 'Build/HackBuild.data.gz',
+    frameworkUrl: 'Build/HackBuild.framework.js.gz',
+    codeUrl: 'Build/HackBuild.wasm.gz'
   })
+
+  const onClickLeft = () => {
+    sendMessage('NetworkBridge', 'Left')
+  }
+
+  const onClickRight = () => {
+    sendMessage('NetworkBridge', 'Right')
+  }
 
   return (
     <div className="relative">
@@ -15,6 +23,13 @@ function App() {
         className="aspect-video size-full"
         // style={{ width: 960, height: 600 }}
       />
+      <div className="absolute inset-0 flex size-full flex-col items-center justify-center p-3">
+        Overlay
+        <div className="flex content-between items-center p-4">
+          <button onClick={onClickLeft}>Left</button>
+          <button onClick={onClickRight}>Right</button>
+        </div>
+      </div>
     </div>
   )
 }
